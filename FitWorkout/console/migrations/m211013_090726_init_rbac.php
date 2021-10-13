62c66e846030843934d70b72bc3bd40b953e22f8
Artur Quaresma <artursquaresma@gmail.com>
2021-10-13 10:10:19 +0100

    rbac configurado
    ' init rbac ' sem roles e permissões

---
+++ FitWorkout/console/migrations/m211013_090726_init_rbac.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m211013_090726_init_rbac
+ */
+class m211013_090726_init_rbac extends Migration
+{
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+    }
+
+    public function down()
+    {
+        echo "m211013_090726_init_rbac cannot be reverted.\n";
+
+        return false;
+    }
+}
