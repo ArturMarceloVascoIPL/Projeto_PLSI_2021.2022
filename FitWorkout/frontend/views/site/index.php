4febad46ba5374c7a52be9991d14c45e912bb3a6
Vasco Silva <2200426@ipleiria.pt>
2021-11-11 17:13:44 +0000

    Frontend(Guest) Fix
    - Página de Index, Login e Registo reparadas.

--- FitWorkout/frontend/views/site/index.php
+++ FitWorkout/frontend/views/site/index.php
@@ -10,103 +10,89 @@ $this->title = 'Fit Workout';
 
 
 <body class="body-wrapper">
-    <div class="site-index">
-        <section>
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="page-header">
-                            <?php
+<div class="site-index">
+    <div class="container">
+        <div class="row">
+            <div class="page-header">
+                <?php
 
-                            if (Yii::$app->user->isGuest)
-                                echo "<h2>Bem Vindo | Inicie a sessão</h2>";
-                            else
-                                echo "<h2>Bem Vindo <b>" . Yii::$app->user->identity->username . "</b></h2><br><br>";
+                if (Yii::$app->user->isGuest)
+                    echo "<h2>Bem Vindo | Inicie a sessão</h2>";
+                else
+                    echo "<h2>Bem Vindo <b>" . Yii::$app->user->identity->username . "</b></h2><br><br>";
 
-                            ?>
+                ?>
+            </div>
 
-                        </div>
+            <div class="jumbotron text-center bg-transparent">
+                <h1 class="display-4">😎</h1>
+                <?php if (Yii::$app->user->isGuest) { ?>
 
-                        <div class="jumbotron text-center bg-transparent">
-                            <h1 class="display-4">😎</h1>
-                            <?php if (Yii::$app->user->isGuest) { ?>
+                <?php } else { ?>
+                    <div class="body-content">
+                        <div class="row">
+                            <div class="col">
+                                <div class="card ">
 
-                            <?php } else { ?>
-                                <div class="body-content">
+                                    <div class="card-header">
+                                        <h2>Encomendas</h2>
+                                    </div>
 
-                                    <div class="row">
-                                        <div class="col">
-                                            <div class="card ">
-                                                <div class="card-header">
-                                                    <h2>Encomendas</h2>
-                                                </div>
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item">Cras justo odio
+                                            <?=
+                                            Html::img('@web/img/truck.svg', $options = ['class' => 'float-right', 'alt' => 'logo']);
+                                            ?>
+                                        </li>
 
-                                                <ul class="list-group list-group-flush">
-                                                    <li class="list-group-item">Cras justo odio
-                                                        <?=
-                                                        Html::img('@web/img/truck.svg', $options = ['class' => 'float-right', 'alt' => 'logo']);
-                                                        ?>
-                                                    </li>
+                                        <li class="list-group-item">Dapibus ac facilisis in
+                                            <?=
+                                            Html::img('@web/img/shop.svg', $options = ['class' => 'float-right', 'alt' => 'logo']);
+                                            ?></li>
+                                        </li>
+                                        <li class="list-group-item">Vestibulum at eros
+                                            <?=
+                                            Html::img('@web/img/shop.svg', $options = ['class' => 'float-right', 'alt' => 'logo']);
+                                            ?></li>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
 
-                                                    <li class="list-group-item">Dapibus ac facilisis in
-                                                        <?=
-                                                        Html::img('@web/img/shop.svg', $options = ['class' => 'float-right', 'alt' => 'logo']);
-                                                        ?></li>
-                                                    </li>
-                                                    <li class="list-group-item">Vestibulum at eros
-                                                        <?=
-                                                        Html::img('@web/img/shop.svg', $options = ['class' => 'float-right', 'alt' => 'logo']);
-                                                        ?></li>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                        </div>
+                            <div class="col">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h2>Plano de Treino de Hoje</h2>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="card-title">
 
-                                        <div class="col">
-                                            <div class="card">
-                                                <div class="card-header">
-                                                    <h2>Plano de Treino de Hoje</h2>
-                                                </div>
-                                                <div class="card-body">
-                                                    <div class="card-title">
-
-                                                    </div>
-                                                </div>
-
-
-                                            </div>
-                                        </div>
-
-                                        <div class="col">
-                                            <div class="card">
-                                                <div class="card-header">
-                                                    <h2>Notificações do Personal Trainer</h2>
-                                                </div>
-
-                                                <div class="card-body">
-                                                    <div class="card-title">Personal Trainer</div>
-                                                    <p class="cart-text">'mensagem do personal trainer'
-                                                        <?=
-                                                        Html::img('@web/img/bell-fill.svg', $options = ['class' => 'float-right', 'alt' => 'logo']);
-                                                        ?></li>
-                                                    </p>
-                                                </div>
-                                            </div>
                                         </div>
                                     </div>
                                 </div>
+                            </div>
 
-                            <?php } ?>
+                            <div class="col">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h2>Notificações do Personal Trainer</h2>
+                                    </div>
+
+                                    <div class="card-body">
+                                        <div class="card-title">Personal Trainer</div>
+                                        <p class="cart-text">'mensagem do personal trainer'
+                                            <?=
+                                            Html::img('@web/img/bell-fill.svg', $options = ['class' => 'float-right', 'alt' => 'logo']);
+                                            ?></li>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-
-                        
-
                     </div>
-                </div>
-
+                <?php } ?>
             </div>
-
-
-        </section>
+        </div>
     </div>
+</div>
 </body>
