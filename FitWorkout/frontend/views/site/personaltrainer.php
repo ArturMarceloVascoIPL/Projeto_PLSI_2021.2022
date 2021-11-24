2a4159c948d30075a5a0369cb302027ee45dc1c4
Marcelo Marques <2200428@my.ipleiria.pt>
2021-11-24 10:08:22 +0000

    FrontEnd UI
    Criado a Interface de Exercicios, Personal Trainer e Loja

--- FitWorkout/frontend/views/site/personaltrainer.php
+++ FitWorkout/frontend/views/site/personaltrainer.php
@@ -9,8 +9,47 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
+    <br><br>
 
-    <p>This is the Personal Trainer page. You may modify the following file to customize its content: </p>
 
-    <code><?= __FILE__ ?></code>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th style="width:30%">Nome</th>
+                <th style="width:60%">Preço</th>
+                <th style="width:10%">Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">Marcelo</th>
+                <td>5€</td>
+                <td><button type="button" class="btn btn-primary btn-sm m-0 p-2" style="background-color: #5672F9;">Contratar</button></td>
+            </tr>
+
+            <tr>
+                <th scope="row">Marcelo</th>
+                <td>5€</td>
+                <td><button type="button" class="btn btn-primary btn-sm m-0 p-2" style="background-color: #5672F9;">Contratar</button></td>
+            </tr>
+
+            <tr>
+                <th scope="row">Marcelo</th>
+                <td>5€</td>
+                <td><button type="button" class="btn btn-primary btn-sm m-0 p-2" style="background-color: #5672F9;">Contratar</button></td>
+            </tr>
+
+            <tr>
+                <th scope="row">Marcelo</th>
+                <td>5€</td>
+                <td><button type="button" class="btn btn-primary btn-sm m-0 p-2" style="background-color: #5672F9;">Contratar</button></td>
+            </tr>
+
+            <tr>
+                <th scope="row">Marcelo</th>
+                <td>5€</td>
+                <td><button type="button" class="btn btn-primary btn-sm m-0 p-2" style="background-color: #5672F9;">Contratar</button></td>
+            </tr>
+        </tbody>
+    </table>
 </div>
