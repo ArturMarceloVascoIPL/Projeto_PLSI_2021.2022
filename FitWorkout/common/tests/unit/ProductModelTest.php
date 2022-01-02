2eb0c86fd470128f44ce4eefdf3c52b3001c2c7b
Vasco Silva <2200426@my.ipleiria.pt>
2022-01-02 22:45:04 +0000

    Ficheiros base de Testes Unitários + Codeception
    - Preparados testes no Modelo ChatMessage, Exercise, Product, PTApplication, User

---
+++ FitWorkout/common/tests/unit/ProductModelTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace common\tests;
+
+class ProductModelTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \common\tests\UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+    }
+}
